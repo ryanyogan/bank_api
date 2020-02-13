d7f8e88fce2ec4fe18623f0b6d67d759d7698ab4
ryanyogan <ryanyogan@gmail.com>
2020-02-13 12:06:21 -0800

    fix up envs a tad

--- config/dev.exs
+++ config/dev.exs
@@ -3,24 +3,6 @@ import Config
 config :eventstore,
   column_data_type: "jsonb"
 
-config :eventstore, EventStore.Storage,
-  serializer: EventStore.JsonbSerializer,
-  types: EventStore.PostgresTypes,
-  username: "postgres",
-  password: "postgres",
-  database: "bank_api_eventstore_dev",
-  hostname: "localhost",
-  pool_size: 10,
-  pool_overflow: 5
-
-config :bank_api, BankAPI.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "bank_api_dev",
-  hostname: "localhost",
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
-
 config :bank_api, BankAPIWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
@@ -51,3 +33,5 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
 config :phoenix, :plug_init_mode, :runtime
+
+import_config "dev.secret.exs"
--- config/test.exs
+++ config/test.exs
@@ -6,15 +6,10 @@ config :commanded,
 config :commanded, Commanded.EventStore.Adapters.InMemory,
   serializer: Commanded.Serialization.JsonSerializer
 
-config :bank_api, BankAPI.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "bank_api_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
-
 config :bank_api, BankAPIWeb.Endpoint,
   http: [port: 4002],
   server: false
 
 config :logger, level: :warn
+
+import_config "test.secret.exs"
